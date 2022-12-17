22d5f15b826daf73fe075f84b1703124d21c466b
AbayMuhanov <98691602+AbayMuhanov@users.noreply.github.com>
2022-12-17 17:37:22 +0600

    my-insertion-sort

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {10, 5, 6, 9, 1, 4, 8, 6, 7, 2, 3,};
+
+            InsertionSort.insertionSort(array);
+        System.out.println(Arrays.toString(array));
+
+        }
+    }
